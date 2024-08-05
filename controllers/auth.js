c7e641bd7a839a95117d88c3be3b1a15ce1d66a1
JavArm94 <javarm94@gmail.com>
2024-08-05 14:06:00 -0300

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,44 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
+
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+
+  // PODEMOS HACES UNA VALIDACION MANUAL O CON MONGOOSE
+  // const { name, email, password } = req.body;
+  // if (!name || !email || !password) {
+  //   throw new BadRequestError("Please provide name, email and password");
+  // }
+  // const { name, email, password } = req.body;
+  // const salt = await bcrypt.genSalt(10); //number bytes
+  // const hashPassword = await bcrypt.hash(password, salt);
+  // const tempUser = { name, email, password: hashPassword };
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+  // compare password
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const isPasswordCorrect = await user.checkPassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+};
+
+module.exports = { register, login };
